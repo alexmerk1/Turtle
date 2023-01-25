9077eedaf88a8598a8765e05554f43e74e11d825
Alexandr <109699943+alexmerk1@users.noreply.github.com>
2023-01-25 23:04:28 +0300

    Add file - code

---
+++ Turtle.main.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtle.title("Красивая спираль")
+turtle.speed(45)
+turtle.bgcolor("black")
+r, g, b = 255, 0, 0
+
+for i in range(255*2):
+    turtle.colormode(255)
+    if i < 255//3:
+        g += 3
+    elif i < 255*2//3:
+        r -= 3
+    elif i < 255:
+        b += 3
+    elif i < 255*4//3:
+        g -= 3
+    elif i < 255*5//3:
+        r += 3
+    else:
+        b -= 3
+    turtle.fd(50+i)
+    turtle.rt(91)
+    turtle.pencolor(r, g, b)
